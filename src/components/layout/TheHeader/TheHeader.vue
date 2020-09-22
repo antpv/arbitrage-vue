a5a3bd29cf7dae37d5ddd4d1aa58e007c63682c8
Antipov <antpv99@gmail.com>
2020-09-22 14:45:59 +0300

    Complex refactoring

--- src/components/layout/TheHeader/TheHeader.vue
+++ src/components/layout/TheHeader/TheHeader.vue
@@ -1,17 +1,16 @@
 <template>
   <vs-navbar color="primary" shadow text-white fixed>
     <template #left>
-      <vs-navbar-item
-        @click="$route.path !== '/statistics' && $router.push('/statistics')"
-        :active="$route.name === 'Statistics'"
-      >
+      <vs-navbar-item to="/statistics" :active="$route.name === 'Statistics'">
         Статистика
       </vs-navbar-item>
-      <vs-navbar-item
-        @click="$route.path !== '/applications' && $router.push('/applications')"
-        :active="$route.name === 'Applications'"
-      >
+      <vs-navbar-item to="/applications" :active="$route.name === 'Applications'">
         Приложения
+      </vs-navbar-item>
+    </template>
+    <template #right>
+      <vs-navbar-item @click="handleLogout">
+        Выйти
       </vs-navbar-item>
     </template>
   </vs-navbar>
@@ -19,6 +18,13 @@
 
 <script>
 export default {
-  name: 'TheHeader'
+  name: 'TheHeader',
+
+  methods: {
+    handleLogout() {
+      this.$store.commit('auth/setAuth', false)
+      this.$router.push({ path: '/login' })
+    }
+  }
 }
 </script>
--- src/pages/applications/Applications.vue
+++ src/pages/applications/Applications.vue
@@ -70,68 +70,16 @@
     </vs-row>
 
     <!-- CREATE APPLICATION -->
-    <vs-dialog width="400px" not-center v-model="addModalVisible">
-      <template #header>
-        <h4 class="not-margin">
-          Новое приложение
-        </h4>
-      </template>
-
-      <div class="form-container">
-        <div class="form-container__row">
-          <vs-input v-model="form.bundle" placeholder="Бандл">
-            <template
-              v-if="formErrors['bundle'] && formErrors['bundle'].length > 0"
-              #message-danger
-            >
-              {{ formErrors['bundle'].join(', ') }}
-            </template>
-          </vs-input>
-        </div>
-
-        <div class="form-container__row">
-          <vs-input v-model="form.offer" placeholder="Оффер">
-            <template v-if="formErrors['offer'] && formErrors['offer'].length > 0" #message-danger>
-              {{ formErrors['offer'].join(', ') }}
-            </template>
-          </vs-input>
-        </div>
-
-        <div class="form-container__row">
-          <vs-input v-model="form.defaultLanguage" placeholder="Дефольный язык">
-            <template
-              v-if="formErrors['defaultLanguage'] && formErrors['defaultLanguage'].length > 0"
-              #message-danger
-            >
-              {{ formErrors['defaultLanguage'].join(', ') }}
-            </template>
-          </vs-input>
-        </div>
-
-        <div class="form-container__row">
-          <vs-input v-model="form.link" placeholder="Ссылка">
-            <template v-if="formErrors['link'] && formErrors['link'].length > 0" #message-danger>
-              {{ formErrors['link'].join(', ') }}
-            </template>
-          </vs-input>
-        </div>
-      </div>
-
-      <template #footer>
-        <div class="con-footer">
-          <vs-button @click="makeApplication" :loading="formRequest" gradient>
-            Создать
-          </vs-button>
-          <vs-button @click="closeAddModal" dark transparent>
-            Отмена
-          </vs-button>
-        </div>
-      </template>
-    </vs-dialog>
+    <MakeApplication
+      :visible="addModalVisible"
+      @close="closeAddModal"
+      @success="payload => applications.push(payload)"
+    />
   </div>
 </template>
 
 <script>
+import MakeApplication from './components/MakeApplication'
 import isValidUrl from '@/utils/isValidUrl'
 
 export default {
@@ -143,10 +91,8 @@ export default {
     }
   },
 
-  watch: {
-    addModalVisible(isVisible) {
-      if (isVisible === false) this.resetForm()
-    }
+  components: {
+    MakeApplication
   },
 
   data() {
@@ -156,25 +102,13 @@ export default {
       currentPage: 1,
       max: 1,
       applications,
-      addModalVisible: false,
-      form: {
-        bundle: '',
-        offer: '',
-        defaultLanguage: '',
-        link: ''
-      },
-      formRequest: false,
-      formErrors: {}
+      addModalVisible: false
     }
   },
 
   computed: {
     tableIsFetched() {
       return this.selectedApplication && this.fetchingApplicationStatistic === false
-    },
-
-    formIsValid() {
-      return Object.values(this.formErrors).every(errors => errors.length === 0)
     }
   },
 
@@ -187,71 +121,7 @@ export default {
 
     closeAddModal() {
       this.addModalVisible = false
-    },
-
-    resetForm() {
-      this.form = {
-        bundle: '',
-        offer: '',
-        defaultLanguage: '',
-        link: ''
-      }
-      this.formErrors = {}
-    },
-
-    validateForm() {
-      const { bundle, offer, defaultLanguage, link } = this.form
-      const errors = {}
-
-      for (const key in this.form) errors[key] = []
-
-      if (!bundle.length) {
-        errors['bundle'].push('Значение обязательно')
-      }
-
-      if (!offer.length) {
-        errors['offer'].push('Значение обязательно')
-      }
-
-      if (!defaultLanguage.length) {
-        errors['defaultLanguage'].push('Значение обязательно')
-      }
-
-      if (!link.length) {
-        errors['link'].push('Значение обязательно')
-      }
-
-      this.formErrors = errors
-    },
-
-    makeApplication() {
-      this.validateForm()
-
-      if (this.formIsValid === false) return
-
-      const payload = Object.assign({}, this.form)
-
-      this.formRequest = true
-
-      setTimeout(() => {
-        this.applications.push(payload)
-        this.formRequest = false
-        this.resetForm()
-        this.closeAddModal()
-      }, 500)
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.form-container {
-  &__row {
-    margin-top: 15px;
-
-    &:first-child {
-      margin-top: 0;
-    }
-  }
-}
-</style>
